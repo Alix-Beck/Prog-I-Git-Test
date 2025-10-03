02e86fa7cc51ea2897113f8819d1ac06350718ab
Alix-Beck <abeckbec9@alumnes.ub.edu>
2025-10-04 00:23:59 +0200

    commit p03

---
+++ P03.java
@@ -0,0 +1,11 @@
+//P03.java
+import java.util.*;
+public class P03 {
+    public static void main(String[] args) {
+        String nom;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Com et dius?");
+        nom = scanner.nextLine();
+        System.out.println("El teu nom és: " + nom);
+    }
+}
