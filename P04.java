bccffeb682642a3cfb72bfbebd627699f1899f7d
Alix-Beck <abeckbec9@alumnes.ub.edu>
2025-10-04 00:29:20 +0200

    commit P04

---
+++ P04.java
@@ -0,0 +1,11 @@
+//P04.java
+import java.util.*;
+public class P04 {
+    public static void main(String[] args) {
+        nom;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Com et dius?");
+        nom = scanner.nextLine();
+        System.out.println("El teu nom és: " + nom);
+    }
+}
